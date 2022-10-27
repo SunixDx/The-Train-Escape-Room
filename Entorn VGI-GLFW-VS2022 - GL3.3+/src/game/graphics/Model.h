fe7fd91344e2155654ef0e0de8558317e227d24a
OriolMarion <oriolmarionescude@gmail.com>
2022-10-27 18:17:03 +0200

    classe Model

---
+++ Entorn VGI-GLFW-VS2022 - GL3.3+/src/game/graphics/Model.h
@@ -0,0 +1,34 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+#include <vector>
+#include <string>
+
+#include "Mesh.h"
+#include "../src/shader.h"
+#include "Texture.h"
+
+class Model
+{
+public:
+	Model(char* path)
+	{
+		loadModel(path);
+	}
+	void Draw(Shader& shader);
+private:
+	// model data
+	std::vector<Mesh> meshes;
+	std::string directory;
+	void loadModel(std::string path);
+	void processNode(aiNode* node, const aiScene* scene);
+	Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+	std::vector<Texture> loadMaterialTextures(aiMaterial* mat,
+		aiTextureType type, std::string typeName);
+};
+
+#endif // !MODEL_H
