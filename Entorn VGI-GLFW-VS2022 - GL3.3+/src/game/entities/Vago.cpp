2e241dce0d36fe49ca5b571314114786b6d19b62
Arnau Marcos Almansa <arnaumarcosalmansa@gmail.com>
2022-11-28 09:39:40 +0100

    quitada pared blanca random

--- Entorn VGI-GLFW-VS2022 - GL3.3+/src/game/entities/Vago.cpp
+++ Entorn VGI-GLFW-VS2022 - GL3.3+/src/game/entities/Vago.cpp
@@ -4,14 +4,14 @@ float Vago::Z_OFFSET = 1.5f;
 
 Vago::Vago(Transform transform, Model* model, GLuint shader_id): GameEntity(transform, nullptr, shader_id)
 {
-	
+	/*
 	my_children.push_back(new GameEntity(Transform(
 		vec3(0.0f, 3.0f, 0.0f),
 		quat(1.0f, 0.0f, 0.0f, 0.0f),
 		vec3(22.0f, 0.1f, 3.0f)
 	), model, my_shader_id));
 	
-	/*
+	
 	my_children.push_back(new GameEntity(Transform(
 		vec3(0.0f, -3.0f, 0.0f),
 		quat(1.0f, 0.0f, 0.0f, 0.0f),
