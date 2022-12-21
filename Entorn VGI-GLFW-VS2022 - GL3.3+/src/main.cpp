4335e9dc7d602d88d8d150fa12c15deb162f9c85
OriolMarion <oriolmarionescude@gmail.com>
2022-12-21 16:42:27 +0100

    eliminado destructor inutil

--- Entorn VGI-GLFW-VS2022 - GL3.3+/src/main.cpp
+++ Entorn VGI-GLFW-VS2022 - GL3.3+/src/main.cpp
@@ -3928,18 +3928,5 @@ int main(void)
 
 	delete Mesh::BASIC_CUBE_MESH;
 
-	// After we are finished, we have to delete the irrKlang Device created earlier
-	// with createIrrKlangDevice(). Use ::drop() to do that. In irrKlang, you should
-	// delete all objects you created with a method or function that starts with 'create'.
-	// (an exception is the play2D()- or play3D()-method, see the documentation or the
-	// next example for an explanation)
-	// The object is deleted simply by calling ->drop().
-
-	/*if (backgroundSound) {
-		backgroundSound->drop();
-	}*/
-	
-	Audio::AUDIO_FUNCTIONS.~Audio();
-
     return 0;
 }
